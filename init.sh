44c625f87c25b44e31ffacd04c0fe9a084f9d294
Andrey-Marin <io_o.i@mail.ru>
2023-02-16 12:00:32 +0300

    Some fix

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
