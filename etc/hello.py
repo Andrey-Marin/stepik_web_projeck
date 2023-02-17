110ca7e6358a8ee321e6f57c287177a46213abff
Andrey-Marin <io_o.i@mail.ru>
2023-02-17 15:50:36 +0300

    Some fix for ex.1.9

---
+++ etc/hello.py
@@ -0,0 +1,5 @@
+# gunicorn configuration file for "hello" python application
+
+pythonpath = '/home/box/web/'
+bind = "0.0.0.0:8080"
+worker = 4
--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ def app(environ, start_response):
 	status='200 OK'
 	headers=[('Content-Type', 'text/plain')]
 	start_response(status, headers)
-	return environ.split('$').strip()
+	return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
