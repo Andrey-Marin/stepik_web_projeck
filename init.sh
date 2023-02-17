6b8614069b492712698585fa2e98185883ea7a22
Andrey-Marin <io_o.i@mail.ru>
2023-02-17 14:53:15 +0300

    Add gunicorn config, add wsgi app (hello.py) and change nginx.conf

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app
